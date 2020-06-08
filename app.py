e84f870811a72eddfbe34a7d8785436542590672
miutamihai <miuta.mihai@gmail.com>
2020-06-08 09:39:09 +0300

    Changed data structures

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import cx_Oracle
 
 app = Flask(__name__)
 
-user_role = 'Client'
-username = ''
+user_data = {
+    'Name': '',
+    'Password': '',
+    'Role': 'Client'
+}
 
 dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCLCDB.localdomain')
 conn = cx_Oracle.connect(user='mihai', password='mihai16', dsn=dsn_tns)
@@ -12,16 +15,15 @@ conn = cx_Oracle.connect(user='mihai', password='mihai16', dsn=dsn_tns)
 
 @app.route('/')
 def index():
-    return render_template('index.html', user_role=user_role, username=username)
+    return render_template('index.html', user_data=user_data)
 
 
 @app.route('/logout/')
 def logout():
-    global username
-    global user_role
-    username = ''
-    user_role = 'Client'
-    return render_template('index.html', username=username, user_role=user_role)
+    global user_data
+    user_data['Name'] = ''
+    user_data['Role'] = 'Client'
+    return render_template('index.html', user_data=user_data)
 
 
 @app.route('/clienti')
@@ -29,7 +31,7 @@ def customers():
     cursor = conn.cursor()
     cursor.execute('''select CUSTOMERID, CUSTOMERNAME, CUSTOMERADDRESS, CUSTOMERCITY, CUSTOMERCOUNTY, CUSTOMERTYPE
         from CUSTOMERTBL''')
-    return render_template('customers.html', data=cursor, user_role=user_role, username=username)
+    return render_template('customers.html', data=cursor, user_data=user_data)
 
 
 @app.route('/locatii')
@@ -39,24 +41,23 @@ def locations():
         from CHARGERTBL
         inner join ELECTRICITYTBL E on CHARGERTBL.ELECTRICITYID = E.ELECTRICITYID
         order by CHARGERID''')
-    return render_template('locations.html', data=cursor, user_role=user_role, username=username)
+    return render_template('locations.html', data=cursor, user_data=user_data)
 
 
 @app.route('/furnizori')
 def suppliers():
     cursor = conn.cursor()
     cursor.execute('select * from MIHAI.ELECTRICITYTBL')
-    return render_template('suppliers.html', data=cursor, user_role=user_role, username=username)
+    return render_template('suppliers.html', data=cursor, user_data=user_data)
 
 
 @app.route('/autentificare', methods=['GET', 'POST'])
 def login():
-    global user_role
+    global user_data
     if request.method == 'GET':
-        return render_template('login.html', user_role=user_role)
+        return render_template('login.html', user_data=user_data)
     else:
         cursor = conn.cursor()
-        global username
         username = request.form['username']
         password = request.form['password']
         cursor.execute('''select ROLENAME from USERROLES
@@ -64,17 +65,18 @@ def login():
             where USERNAME= :userName and USERPASSWORD=:userPassword''', userName=username, userPassword=password)
         res = cursor.fetchone()
         if res is None:
-            username = ''
             return render_template('login_fail.html')
         else:
-            user_role = res[0]
-            return render_template('index.html', user_role=user_role, username=username)
+            user_data['Name'] = username
+            user_data['Password'] = password
+            user_data['Role'] = res[0]
+            return render_template('index.html', user_data=user_data)
 
 
 @app.route('/comenzi', methods=['GET', 'POST'])
 def orders():
     if request.method == 'GET':
-        return render_template('orders.html', user_role=user_role, data='')
+        return render_template('orders.html', user_data=user_data, data='')
     else:
         client = int(request.form['client'])
         cursor = conn.cursor()
@@ -84,7 +86,7 @@ def orders():
             inner join VEHICLETBL V on CHARGINGDETAILSTBL.VEHICLEID = V.VEHICLEID
             where CUSTOMERID = :clientId''', clientId=client)
         print(cursor.fetchone())
-        return render_template('orders.html', user_role=user_role, data=cursor, client=client)
+        return render_template('orders.html', user_data=user_data, data=cursor, client=client)
 
 
 if __name__ == '__main__':
